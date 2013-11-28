1516fe6a213ab51fc32ef686583b8e8c8cd3d758
bormansquirrel <bormansquirrel@gmail.com>
2013-11-28 21:37:02 +0000

    fixed copy-paste error

--- src/ios/WaitingDialog.h
+++ src/ios/WaitingDialog.h
@@ -6,18 +6,12 @@
 //
 
 #import <Foundation/Foundation.h>
-#import <Cordova/CDVPlugin.h>
+#import <Cordova/CDV.h>
+//#import <Cordova/CDVPlugin.h>
 
+@interface WaitingDialog : CDVPlugin
 
-@interface WaitingDialog : CDVPlugin {
-    
-}
-
-// UNCOMMENT THIS METHOD if you want to use the plugin with versions of cordova < 2.2.0
-//- (void) show:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options;
-
-// COMMENT THIS METHOD if you want to use the plugin with versions of cordova < 2.2.0
-- (void) show:(CDVInvokedUrlCommand*)command;
-- (void) hide:(CDVInvokedUrlCommand*)command;
+- (void)show:(CDVInvokedUrlCommand*)command;
+- (void)hide:(CDVInvokedUrlCommand*)command;
 
 @end
